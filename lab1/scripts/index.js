81f2dcc8cc9509acc94fe0076c838ac73bb6bcc5
Jenny Su <71398958+jennysu1105@users.noreply.github.com>
2023-01-23 16:56:00 -0500

    Mony's work

---
+++ lab1/scripts/index.js
@@ -0,0 +1,42 @@
+//Mony Thach 500695151
+
+
+
+
+//
+$(document).ready(function(){
+    //fade functions for devices
+    $("#galaxys7In").click(function(){
+        $("#phoneImg").fadeIn();
+    });
+    $("#galaxys7Out").click(function(){
+        $("#phoneImg").fadeOut();
+    });
+    $("#surfaceIn").click(function(){
+        $("#compImg").fadeIn();
+    });
+    $("#surfaceOut").click(function(){
+        $("#compImg").fadeOut();
+    });
+    $("#macbookIn").click(function(){
+        $("#noteImg").fadeIn();
+    });
+    $("#macbookOut").click(function(){
+        $("#noteImg").fadeOut();
+    });
+    //fade function for info
+    $("#infoIn").click(function(){
+        $(".information").fadeIn();
+    });
+    $("#infoOut").click(function(){
+        $(".information").fadeOut();
+    });
+    //animation function to toggle height to 0
+    $(".animate button").click(function(){
+        $(".images img").animate(
+            {
+            height: 'toggle'
+            }
+        );
+    });
+});
