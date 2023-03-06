47d22f0be8a1405f3c5862e01401262f1952d289
Tiffany Tran <tiffanytran0143@gmail.com>
2023-03-05 20:42:26 -0500

    fixing an oops

---
+++ project/scripts/index.js
@@ -0,0 +1,28 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    var linebreak = document.createElement("br");
+    var data = ev.dataTransfer.getData("text");
+    var item = document.getElementById("item_" + data);
+    var item_price = document.getElementById("price_" + data);
+
+    ev.preventDefault();
+    ev.target.append(item.innerHTML + " - $" + item_price.innerHTML);
+    ev.target.appendChild(linebreak);
+
+    var cart_total = document.getElementById("cart_total");
+    var price = Number(item_price.innerHTML);
+
+    try {
+        cart_total.innerHTML = Number(cart_total.innerHTML) + price;
+    }
+    catch {
+        cart_total.innerHTML = price;
+    }
+}
