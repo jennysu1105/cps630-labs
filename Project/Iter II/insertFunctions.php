8629f220635fa75ac6c380f7057c4d0894cda9a4
Michael <michaelrevi19@gmail.com>
2023-03-14 18:43:48 -0400

    added insert, select, delete functions

--- Project/Iter II/insertFunctions.php
+++ Project/Iter II/insertFunctions.php
@@ -1,6 +1,6 @@
 <?php
     include_once "dbConnection.php";
-    function submitQuery($query) {
+    function submitInsertQuery($query) {
         global $connect; //from "dbConnection.php"
         try {
             $connect->query($query);
@@ -8,40 +8,39 @@
             return 1;
         }
         catch (Exception $e) {
-            echo "Error creating new record: ".$e->getMessage();
+            //echo "Error creating new record: ".$e->getMessage();
             return 0;
         }
-        $connect->close();
     }
 
     function insertShopping ($store_code, $total_price) {
         $insertShopping="INSERT INTO shoppingTable (store_code, total_price) VALUES ($store_code, $total_price)";
-        return submitQuery($insertShopping);
+        return submitInsertQuery($insertShopping);
     }
     
     function insertTruck ($truck_code, $availability_code) {
         $insertTruck="INSERT INTO truckTable (truck_code, availability_code) VALUES ($truck_code, $availability_code)";
-        return submitQuery($insertTruck);
+        return submitInsertQuery($insertTruck);
     }
 
     function insertTrip ($source_code, $destination_code, $distance, $truck_id, $price) {
         $insertTrip="INSERT INTO tripTable (source_code, destination_code, distance, truck_id, price) VALUES ('$source_code', '$destination_code', $distance, $truck_id, $price)";
-        return submitQuery($insertTrip);
+        return submitInsertQuery($insertTrip);
     }
 
     function insertUser ($full_name, $telephone, $email, $home_address, $city_code, $login_id, $user_password, $balance) {
         $insertUser="INSERT INTO userTable (full_name, telephone, email, home_address, city_code, login_id, user_password, balance) VALUES ('$full_name', '$telephone', '$email', '$home_address', '$city_code', '$login_id', '$user_password', $balance)";
-        return submitQuery($insertUser);
+        return submitInsertQuery($insertUser);
     }
 
     function insertItem ($item_name, $item_price, $made_in, $department_code) {
         $insertItem="INSERT INTO itemTable (item_name, item_price, made_in, department_code) VALUES ('$item_name', $item_price, '$made_in', '$department_code')";
-        return submitQuery ($insertItem);
+        return submitInsertQuery ($insertItem);
     }
 
     function insertOrder ($date_issued, $date_received, $total_price, $payment_code, $user_id, $trip_id, $receipt_id) {
         $insertOrder="INSERT INTO orderTable (date_issued, date_received, total_price, payment_code, user_id, trip_id, receipt_id) VALUES ('$date_issued', '$date_received', $total_price, '$payment_code', $user_id, $trip_id, $receipt_id)";
-        return submitQuery($insertOrder);
+        return submitInsertQuery($insertOrder);
 
     }
 ?>
