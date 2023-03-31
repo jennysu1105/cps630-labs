986cbfd6e5418f83bd7f992d6cf0f6d93f01c21a
Jenny Su <71398958+jennysu1105@users.noreply.github.com>
2023-03-31 19:07:57 -0400

    remove hard-coded date for submit

--- Project/react-app/php/submitOrder.php
+++ Project/react-app/php/submitOrder.php
@@ -8,8 +8,9 @@ include_once "Models.php";
 $payment = $_GET['payment'];
 $total = $_GET['total'];
 $user_id = $_GET['user'];
+$date = date('Y-m-d');
 
-$order = new Order('2023-03-27', '2023-03-27', $total, $payment, $user_id, 1, 1);
+$order = new Order($date, $date, $total, $payment, $user_id, 1, 1);
 $order->insert();
 $query = "SELECT MAX(order_id) FROM orderTable";
 $result = submitSelectQuery($query);
