36a2a54b2b30ae863337177beb19fe04b8428bc9
Tiffany Tran <tiffany.tran@torontomu.ca>
2023-03-30 21:16:32 -0400

    admin mode added

--- Project/react-app/php/Table/populateRecords.php
+++ Project/react-app/php/Table/populateRecords.php
@@ -13,7 +13,7 @@
     $trip2 = new Trip("L5P1B2", "M5B2K3", 3.2, 2, 60.84);
 
     $salt1 = generateSalt(); $salt2 = generateSalt(); $salt3 = generateSalt(); $salt4 = generateSalt();
-    $admin = new User("Michael Widianto", "4169996666", "michael.r.widianto@torontomu.ca", "10 Yonge St", "416", "mrw19", $salt1, hashPassword("admin".$salt1), 0);
+    $admin = new User("Michael Widianto", "4169996666", "michael.r.widianto@torontomu.ca", "10 Yonge St", "416", "mrw19", $salt1, hashPassword("adminuser".$salt1), 0);
     $user2 = new User("John Doe", "4163334444", "johndoe@gmail.com", "10 Adeline St", "416", "jdoe", $salt2, hashPassword("password".$salt2), 500);
     $user3 = new User("Alex Stuart", "7063334444", "astuart@gmail.com", "22 Yonge St", "706", "astuart", $salt3, hashPassword("password123".$salt3), 200);
     $user4 = new User("Bill Clint", "5121112222", "bclint@gmail.com", "45 Temprest St", "512", "blint", $salt4, hashPassword("password456".$salt4), 100);
--- Project/react-app/src/components/Navigation.jsx
+++ Project/react-app/src/components/Navigation.jsx
@@ -4,8 +4,43 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../static/img/system_logo.jpg'
+import { useCookies } from 'react-cookie';
 
 const Navigation = () => {
+    const [cookies, setCookie] = useCookies(['user']);
+
+    if (cookies.user != 1) {
+        return (
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                            src={logo}
+                            width="28"
+                            height="28"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        SCS
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/home">Home</Nav.Link>
+                            <Nav.Link href="/about_us">About us</Nav.Link>
+                            <Nav.Link href="/contact_us">Contact us</Nav.Link>
+                            <Nav.Link href="/types_of_services">Types of services</Nav.Link>
+                            <Nav.Link href="/reviews">Reviews</Nav.Link>
+                            <Nav.Link href="/shopping_cart">Shopping cart</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="/">Logout</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
+
     return (
         <Navbar bg="light" expand="lg">
             <Container>
