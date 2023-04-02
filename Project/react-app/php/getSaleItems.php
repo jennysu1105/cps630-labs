f294e1eb2c82e9b52e45d90f3b05da7ad9e33e41
Tiffany Tran <tiffanytran0143@gmail.com>
2023-04-01 20:17:01 -0400

    fix image on fire sale screen

--- Project/react-app/php/getSaleItems.php
+++ Project/react-app/php/getSaleItems.php
@@ -2,7 +2,7 @@
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: *");
     include_once "submitQuery.php";
-    $query = "SELECT itemtable.item_name, itemtable.item_price, itemsaletable.sale_price FROM itemtable INNER JOIN itemsaletable ON itemsaletable.item_id = itemtable.item_id;";
+    $query = "SELECT itemtable.item_name, itemtable.item_price, itemtable.image_name, itemsaletable.sale_price FROM itemtable INNER JOIN itemsaletable ON itemsaletable.item_id = itemtable.item_id;";
     $items = submitSelectQuery($query);
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode($items);
--- Project/react-app/src/components/Index.jsx
+++ Project/react-app/src/components/Index.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import shirt from '../static/img/shirt.jpg';
 import {useCookies} from 'react-cookie';
 
 const Index = () => {
